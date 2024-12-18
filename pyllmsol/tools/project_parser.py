f74f81cfe5b9fd18c0d0f669f69b433e3c2f6943
ArthurBernard <arthur.bernard.92@gmail.com>
2024-12-18 17:16:42 +0100

    update parser object

--- pyllmsol/tools/project_parser.py
+++ pyllmsol/tools/project_parser.py
@@ -4,16 +4,15 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2024-12-15 09:55:05
 # @Last modified by: ArthurBernard
-# @Last modified time: 2024-12-17 16:41:04
+# @Last modified time: 2024-12-18 17:16:10
 # @File path:./pyllmsol/tools/project_parser.py
 # @Project: PyLLMSol
 
 """ Parser objects. """
 
 # Built-in packages
-# import os
 from pathlib import Path
-# import re
+import re
 
 # Third party packages
 
@@ -29,19 +28,83 @@ class FileParser:
         self.path = Path(path) if isinstance(path, str) else path
         self.content = self.path.read_text(encoding='utf-8')
 
-    def parse(self):
-        """ Parse code. """
-        raise NotImplementedError("Subclasses must implement the parse method.")
+    def parse(self) -> dict:
+        """ Parse file. """
+        return {"content": self.content, "metadata": {"path": self.path}}
 
-    def __call__(self, header: bool = True):
+    def __call__(self, header: bool = True) -> str:
         """ Reformat text file. """
         text = ""
 
         if header:
-            text += f"## Header\npath: {self.path}\n"
+            text += f"### Header\npath: {self.path}\n"
 
-        text += f"##Content\n{self.content}"
+        text += f"### Content\n{self.content}"
 
+        return text
+
+
+class PythonParser(FileParser):
+    """ Class to parse python code. """
+    def __init__(self, path: str | Path):
+        self.path = Path(path) if isinstance(path, str) else path
+
+        if self.path.suffix != ".py":
+            raise ValueError("File must be a Python code.")
+
+        self.content = self.path.read_text(encoding='utf-8')
+
+    def __call__(self, header: bool = True, rm_comments: bool = False) -> str:
+        """ Reformat text file. """
+        text = "## Python script\n"
+
+        if header:
+            text += f"### Header\npath: {self.path}\n"
+
+        # Remove extra blank lines
+        content = re.sub(r"\n\s*\n", "\n", self.content)
+
+        if rm_comments:
+            # Remove comments
+            content = re.sub(r"(?m)^\s*#.*$", "", content)
+
+        text += f"### Content\n{content}"
+
+        return text
+
+
+class ProjectParser:
+    """ Base class for parsing different parts of a project. """
+    def __init__(self, path: str | Path):
+        self.repo_path = Path(path) if isinstance(path, str) else path
+
+    def parse(self):
+        """ Parse project. """
+        parsed_readme = FileParser(self.repo_path / "README.md").parse()
+        parsed_python_code = [
+            PythonParser(f).parse() for f in self.repo_path.rglob("*.py")
+        ]
+
+        return {
+            "name": self.repo_path.name,
+            "content": [parsed_readme] + parsed_python_code,
+        }
+
+    def __call__(self, header: bool = True, rm_comments: bool = False) -> str:
+        """ Reformat text file. """
+        content = f"# Parsed project '{self.repo_path.name}'\n"
+        readme_path = self.repo_path / "README.md"
+
+        if readme_path.exists():
+            readme_parser = FileParser(readme_path)
+            content += "## README.md file\n"
+            content += readme_parser(header=header)
+
+        for py_file in self.repo_path.rglob("*.py"):
+            python_parser = PythonParser(py_file)
+            content += python_parser(header=header, rm_comments=rm_comments)
+
+        return content
 
 # class ProjectParser:
 #     """ Base class for parsing different parts of a project. """
