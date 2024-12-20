c0b185fdd3a61bcf5d6e0e86bfd42a7e0088e16a
ArthurBernard <arthur.bernard.92@gmail.com>
2024-12-20 09:16:03 +0100

    update project parser

--- pyllmsol/tools/project_parser.py
+++ pyllmsol/tools/project_parser.py
@@ -4,20 +4,20 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2024-12-15 09:55:05
 # @Last modified by: ArthurBernard
-# @Last modified time: 2024-12-18 17:16:10
+# @Last modified time: 2024-12-20 09:15:15
 # @File path:./pyllmsol/tools/project_parser.py
 # @Project: PyLLMSol
 
 """ Parser objects. """
 
 # Built-in packages
+import fnmatch
 from pathlib import Path
 import re
 
 # Third party packages
 
 # Local packages
-from pyllmsol.tools.code_summarizer import download_github_repo
 
 __all__ = []
 
@@ -26,11 +26,15 @@ class FileParser:
     """ Base class for parsing different parts of a project. """
     def __init__(self, path: str | Path):
         self.path = Path(path) if isinstance(path, str) else path
+
+        if not self.path.exists():
+            raise FileNotFoundError(f"File '{self.path}' does not exist.")
+
         self.content = self.path.read_text(encoding='utf-8')
 
     def parse(self) -> dict:
         """ Parse file. """
-        return {"content": self.content, "metadata": {"path": self.path}}
+        return {"content": self.content, "metadata": {"path": str(self.path)}}
 
     def __call__(self, header: bool = True) -> str:
         """ Reformat text file. """
@@ -47,26 +51,24 @@ class FileParser:
 class PythonParser(FileParser):
     """ Class to parse python code. """
     def __init__(self, path: str | Path):
-        self.path = Path(path) if isinstance(path, str) else path
-
+        super().__init__(path)
         if self.path.suffix != ".py":
-            raise ValueError("File must be a Python code.")
+            raise ValueError(f"File '{self.path}' is not a Python file.")
 
-        self.content = self.path.read_text(encoding='utf-8')
-
-    def __call__(self, header: bool = True, rm_comments: bool = False) -> str:
+    def __call__(self, header: bool = True, remove_comments: bool = True) -> str:
         """ Reformat text file. """
         text = "## Python script\n"
 
         if header:
             text += f"### Header\npath: {self.path}\n"
 
-        # Remove extra blank lines
-        content = re.sub(r"\n\s*\n", "\n", self.content)
+        content = self.content
 
-        if rm_comments:
-            # Remove comments
+        if remove_comments:
             content = re.sub(r"(?m)^\s*#.*$", "", content)
+
+        # Remove extra blank lines
+        content = re.sub(r"\n\s*\n", "\n", content)
 
         text += f"### Content\n{content}"
 
@@ -74,117 +76,110 @@ class PythonParser(FileParser):
 
 
 class ProjectParser:
-    """ Base class for parsing different parts of a project. """
-    def __init__(self, path: str | Path):
-        self.repo_path = Path(path) if isinstance(path, str) else path
+    """ Parser for projects containing multiple files. """
 
-    def parse(self):
-        """ Parse project. """
-        parsed_readme = FileParser(self.repo_path / "README.md").parse()
-        parsed_python_code = [
-            PythonParser(f).parse() for f in self.repo_path.rglob("*.py")
-        ]
+    def __init__(self, path: str | Path, include_patterns: list = None):
+        self.repo_path = Path(path) if isinstance(path, str) else path
+        if not self.repo_path.exists():
+            raise FileNotFoundError(f"Path '{self.repo_path}' does not exist.")
+
+        # Default include patterns: README.md and Python files
+        self.include_patterns = include_patterns or ["README.md", "*.py"]
+
+    def _is_included(self, item: Path) -> bool:
+        """ Check if a file matches the include patterns. """
+        return any(fnmatch.fnmatch(item.name, p) for p in self.include_patterns)
+
+    def _contains_included_files(self, directory: Path) -> bool:
+        """ Check if a directory contains any file or directory matching patterns.
+
+        """
+        for item in directory.iterdir():
+            if item.is_file() and self._is_included(item):
+                return True
+
+            if item.is_dir() and self._contains_included_files(item):
+                return True
+
+        return False
+
+    def parse(self) -> dict:
+        """Parse the project directory based on include patterns."""
+        parsed_files = []
+        for item in self.repo_path.rglob("*"):
+            if item.is_file() and self._is_included(item):
+                if item.name == "README.md":
+                    parsed_files.append(FileParser(item).parse())
+
+                elif item.suffix == ".py":
+                    parsed_files.append(PythonParser(item).parse())
 
         return {
-            "name": self.repo_path.name,
-            "content": [parsed_readme] + parsed_python_code,
+            "project_name": self.repo_path.name,
+            "files": parsed_files,
         }
 
-    def __call__(self, header: bool = True, rm_comments: bool = False) -> str:
-        """ Reformat text file. """
-        content = f"# Parsed project '{self.repo_path.name}'\n"
-        readme_path = self.repo_path / "README.md"
+    def tree(self, max_depth: int = None, level: int = 0) -> str:
+        """ Get the project directory tree, keeping only specific file types.
 
-        if readme_path.exists():
-            readme_parser = FileParser(readme_path)
-            content += "## README.md file\n"
-            content += readme_parser(header=header)
+        """
+        tree = ""
+        if level == 0:
+            tree += f"{self.repo_path.name}/\n"
 
-        for py_file in self.repo_path.rglob("*.py"):
-            python_parser = PythonParser(py_file)
-            content += python_parser(header=header, rm_comments=rm_comments)
+        spacer = "  " * level
+        for item in self.repo_path.iterdir():
+            # Skip files/directories that are not included
+            if item.is_file() and not self._is_included(item):
+                continue
+
+            if item.is_dir() and not self._contains_included_files(item):
+                continue
+
+            if item.is_dir():
+                tree += f"{spacer}|-- {item.name}/\n"
+                if max_depth is None or level < max_depth - 1:
+                    pp = ProjectParser(item, self.include_patterns)
+                    tree += pp.tree(max_depth, level + 1)
+
+            elif item.is_file():
+                tree += f"{spacer}|-- {item.name}\n"
+
+        return tree
+
+    def __call__(
+        self,
+        header: bool = True,
+        remove_comments: bool = True,
+    ) -> str:
+        """ Format the entire project as text. """
+        content = f"# Project: {self.repo_path.name}\n"
+
+        for item in self.repo_path.rglob("*"):
+            if item.is_file() and self._is_included(item):
+                if item.name == "README.md":
+                    content += "\n## README.md\n"
+                    content += FileParser(item)(header=header)
+
+                elif item.suffix == ".py":
+                    content += PythonParser(item)(
+                        header=header,
+                        remove_comments=remove_comments
+                    )
 
         return content
 
-# class ProjectParser:
-#     """ Base class for parsing different parts of a project. """
-#     def __init__(self, path: str | Path):
-#         self.repo_path = Path(path) if isinstance(path, str) else path
-
-#     def parse(self):
-#         """ Parse code. """
-#         raise NotImplementedError("Subclasses must implement the parse method.")
-
-
-# class ReadmeParser(ProjectParser):
-#     """ Parser for extracting and standardizing README content. """
-#     def parse(self):
-#         """ Parse README.md file. """
-#         readme_path = self.repo_path / "README.md"
-
-#         if readme_path.exists():
-#             content = readme_path.read_text(encoding='utf-8')
-
-#             return self.reformat(content, "README.md")
-
-#         return "No README.md found.\n"
-
-#     @staticmethod
-#     def reformat(content: str, file_name: str):
-#         """ Reformats README content. """
-#         return f"### {file_name} Content\n{content.strip()}\n"
-
-
-# class PythonCodeParser(ProjectParser):
-#     """Parser for extracting and standardizing Python code content."""
-#     def parse(self):
-#         python_files_content = "### Python Files Content\n"
-#         for py_file in self.repo_path.rglob("*.py"):
-#             content = py_file.read_text(encoding='utf-8')
-#             formatted_content = self.reformat(content, py_file.relative_to(self.repo_path))
-#             python_files_content += formatted_content
-#         return python_files_content
-
-#     @staticmethod
-#     def reformat(content, file_name):
-#         """Reformats Python code content."""
-#         # Remove excessive empty lines and comments for readability
-#         cleaned_content = re.sub(r"(?m)^\s*#.*$", "", content)  # Remove comments
-#         cleaned_content = re.sub(r"\n\s*\n", "\n", cleaned_content)  # Remove extra blank lines
-#         return f"\n#### {file_name}\n{cleaned_content.strip()}\n"
-
-
-# class SummaryGenerator:
-#     """Generates a standardized summary of the project."""
-#     def __init__(self, parsers):
-#         self.parsers = parsers
-
-#     def generate(self):
-#         summary = "### Project Summary\n"
-#         for parser in self.parsers:
-#             summary += parser.parse()
-#         return summary
-
-#     def save(self, output_file, summary):
-#         with open(output_file, 'w', encoding='utf-8') as f:
-#             f.write(summary)
-#         print(f"Summary file created: {output_file}")
-
 
 if __name__ == "__main__":
-    # Clone repository
-    repo_url = input("Enter the GitHub repository URL: ")
-    repo_path = download_github_repo(repo_url)
+    # Set project parser
+    project_path = Path("../PyLLMSol")
+    print(f"Parse project at {project_path}")
 
-    # Set up parsers
-    # parsers = [
-    #     ReadmeParser(repo_path),
-    #     PythonCodeParser(repo_path),
-    # ]
+    project_parser = ProjectParser(project_path)
 
-    # # Generate summary
-    # generator = SummaryGenerator(parsers)
-    # summary = generator.generate()
+    print(project_parser.tree())
 
-    # # Save summary to file
-    # generator.save("summary.txt", summary)
+    parsed_content = project_parser()
+
+    print(f"Parsed project:\n\n{parsed_content[:200]}")
+    print(f"...\n{parsed_content[-200:]}")
